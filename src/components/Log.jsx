cfeda11487e5ec2285c8cb542f69f8cc5c241c12
PresleyWong <presley_wg@yahoo.com>
2023-03-01 17:20:36 +0800

    fix log window height

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,7 +2,7 @@ const Log = ({ logMessage }) => {
   return (
     <div
       id="logWin"
-      className="border-2 px-2 bg-black text-white overflow-auto min-h-[100px] md:h-[480px] md:w-[200px] break-words"
+      className="border-2 px-2 bg-black text-white overflow-auto h-[100px] md:h-[480px] md:w-[200px] break-words"
     >
       <ul className="flex flex-col-reverse gap-y-[5px] text-sm">
         {logMessage.map((item, index) => (
