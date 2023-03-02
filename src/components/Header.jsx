633435566fe9cdc9c729f2a5662fb6037ff03421
PresleyWong <presley_wg@yahoo.com>
2023-03-02 14:16:32 +0800

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Popover } from "@headlessui/react";
 const Header = () => {
   return (
     <Popover className="relative bg-white">
-      <div className="mx-auto max-w-7xl px-6">
-        <div className="flex items-center justify-between border-b-2 border-gray-100  mb-2 md:justify-start md:space-x-10">
+      <div className="mx-auto px-6 py-3 border-b">
+        <div className="flex items-center justify-between border-gray-300 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
               <img
